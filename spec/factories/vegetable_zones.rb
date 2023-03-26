6598a7410022ac400aa065cec44b30481f9f9790
Lacey Weaver <jlweave89@gmail.com>
2023-03-26 12:45:35 -0600

    Add vegetable zone

---
+++ spec/factories/vegetable_zones.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :vegetable_zone do
+    vegetable { nil }
+    zone { "MyString" }
+    plant_seeds_indoors_start { "MyString" }
+    plant_seeds_indoors_end { "MyString" }
+    plant_seedlings_start { "MyString" }
+    plant_seedlings_end { "MyString" }
+    plant_seeds_outdoors_start { "MyString" }
+    plant_seeds_outdoors_end { "MyString" }
+  end
+end
