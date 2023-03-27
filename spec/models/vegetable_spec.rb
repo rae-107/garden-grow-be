b78250552b47d463d833b23f9b58140362fc60af
Lacey Weaver <jlweave89@gmail.com>
2023-03-27 14:35:02 -0600

    Add relationships for vegetable   Changes to be committed: 	modified:   spec/models/vegetable_spec.rb   have_many vegetable_users   have_many users through vegetable_users

--- spec/models/vegetable_spec.rb
+++ spec/models/vegetable_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Vegetable, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "relationships" do
+    it { should have_many :vegatable_users }   
+    it { should have_many(:users).through(:vegatable_users) }   
+  end
 end
