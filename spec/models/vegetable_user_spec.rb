df76b2051eb5f18457a8a15c731c607dd3436be5
Lacey Weaver <jlweave89@gmail.com>
2023-03-27 14:38:02 -0600

    Add relationship test for vegetable_user Changes to be committed: 	modified:   spec/models/vegetable_user_spec.rb   belongs_to user and vegetable

--- spec/models/vegetable_user_spec.rb
+++ spec/models/vegetable_user_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe VegetableUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belongs_to :user }
+  it { should belongs_to :vegetable }
 end
