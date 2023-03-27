5b06ae41d730deaa34253ec946f7a47b4a9a35bb
Alex Pitzel <pitzelalex@gmail.com>
2023-03-27 17:32:48 -0400

    Add vegetables_by_zipcode query Adds the vegetables_by_zipcode field and resolver Adds the vegetable, vegetable_zone and zipcode_result types Adds the test for the vegetables_by_zipcode query Updates the vegetables csv to fix quotation marks in arugula entry Adds support files for the graphql tests Adds/updates config for factory bot and vcr

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,10 +8,18 @@ module Types
     # They will be entry points for queries on your schema.
 
     # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :vegetables_by_zipcode, Types::ZipcodeResultType, null: false do
+      description 'Returns a zone and basic vegetable details'
+      argument :zipcode, String, required: true
+    end
+
+    def vegetables_by_zipcode(args)
+      zone = GrowZoneFacade.get_zone(args[:zipcode])
+
+      {
+        grow_zone: zone, # temp while service is built
+        vegetables: Vegetable.all
+      }
     end
   end
 end
---
+++ spec/requests/vegetables_by_zipcode_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe 'vegetables_by_zipcode', type: :request do
+  it 'returns a list of growing zone and vegetables by zipcode', :vcr do
+    veg1 = create(:vegetable, name: 'Arugula', image: 'arugula.jpg')
+    veg2 = create(:vegetable, name: 'Tomato', image: 'tomato.jpg')
+
+    veggies = query_vegetables_by_zipcode('80817')
+
+    expect(response).to be_successful
+
+    expect(veggies[:data][:vegetablesByZipcode][:growZone]).to eq('6a')
+    expect(veggies[:data][:vegetablesByZipcode][:vegetables]).to be_an Array
+    expect(veggies[:data][:vegetablesByZipcode][:vegetables].count).to eq(2)
+    expect(veggies[:data][:vegetablesByZipcode][:vegetables][0][:id]).to eq(veg1.id.to_s)
+    expect(veggies[:data][:vegetablesByZipcode][:vegetables][0][:name]).to eq('Arugula')
+    expect(veggies[:data][:vegetablesByZipcode][:vegetables][0][:image]).to eq('arugula.jpg')
+    expect(veggies[:data][:vegetablesByZipcode][:vegetables][1][:id]).to eq(veg2.id.to_s)
+    expect(veggies[:data][:vegetablesByZipcode][:vegetables][1][:name]).to eq('Tomato')
+    expect(veggies[:data][:vegetablesByZipcode][:vegetables][1][:image]).to eq('tomato.jpg')
+  end
+
+  private
+
+  def query_vegetables_by_zipcode(zipcode)
+    gql <<-GQL
+    query{
+      vegetablesByZipcode(
+          zipcode: "#{zipcode}"
+          ) {
+             growZone
+             vegetables {
+                 id
+                 name
+                 image
+             }
+      }
+  }
+    GQL
+  end
+end
