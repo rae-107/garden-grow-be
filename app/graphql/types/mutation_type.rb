c702aaf99e50c050544432f6b4fda3d84c474c17
Lacey Weaver <jlweave89@gmail.com>
2023-04-03 14:20:49 -0600

    Add field for the update_vegetable_user method Changes to be committed: 	modified:   app/graphql/types/mutation_type.rb

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_vegetable_user, mutation: Mutations::CreateVegetableUser
+    field :update_vegetable_user, mutation: Mutations::UpdateVegetableUser
   end
 end
