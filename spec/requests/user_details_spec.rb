9eece5946bd3c8d9ba0cd731241eebc601ae72d2
Lacey Weaver <jlweave89@gmail.com>
2023-04-05 12:58:20 -0600

    Add testing for spec file

--- spec/requests/user_details_spec.rb
+++ spec/requests/user_details_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe 'vegetable_details', type: :request do
+RSpec.describe 'user_details', type: :request do
   it 'returns details of a specific user', :vcr do
-    user1 = create(:user) 
+    user1 = create(:user)
     user2 = create(:user)
-    vegetable1 = create(:vegetable) 
+    vegetable1 = create(:vegetable)
     veg_user = create(:vegetable_user, user: user1, vegetable: vegetable1)
 
     user = query_user_details(user1.id, user1.grow_zone)
 
     expect(response).to be_successful
-    
+
     expect(user[:data][:userDetails][:name]).to be_a String
     expect(user[:data][:userDetails][:name]).to eq(user1.name)
     expect(user[:data][:userDetails][:name]).to_not eq(user2.name)
@@ -22,6 +22,34 @@ RSpec.describe 'vegetable_details', type: :request do
     expect(user[:data][:userDetails][:aboutMe]).to eq(user1.about_me)
     expect(user[:data][:userDetails][:vegetableUsers]).to be_an Array
     expect(user[:data][:userDetails][:vegetableUsers].first[:vegetable][:name]).to eq(vegetable1.name)
+  end
+
+  describe 'UserDetailsQuery', type: :request do
+    describe 'POST /graphql' do
+      context 'with missing userId' do
+        it 'returns an error' do
+          # Execute the query with missing userId
+          query_user_details_null
+
+          # Assert that the response contains errors
+          json_response = JSON.parse(response.body)
+          expect(response).to have_http_status(200)
+          expect(json_response).to have_key('errors')
+        end
+      end
+
+      context 'with invalid userId' do
+        it 'returns an error' do
+          # Execute the query with invalid userId
+          query_user_details('invalid', '5a')
+
+          # Assert that the response contains errors
+          json_response = JSON.parse(response.body)
+          expect(response).to have_http_status(200)
+          expect(json_response).to have_key('errors')
+        end
+      end
+    end
   end
 
   private
@@ -44,6 +72,24 @@ RSpec.describe 'vegetable_details', type: :request do
                 name
               }
              }
+      }
+  }
+    GQL
+  end
+
+  def query_user_details_null
+    gql <<-GQL
+    query{
+      userDetails(
+          userId: null
+          ) {
+             name
+             growZone
+             img
+             linkedIn
+             github
+             email
+             aboutMe
       }
   }
     GQL
